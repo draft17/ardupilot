c41cf6bd97a00138f8682cbbb4e9aec0b91b2846
Office Desktop WSL <jhkang@soomvi.com>
2024-02-05 09:37:16 +0900

    56_swarm_FixedWing initial Commit

--- ArduPlane/version.h
+++ ArduPlane/version.h
@@ -6,7 +6,7 @@
 
 #include "ap_version.h"
 
-#define THISFIRMWARE "ArduPlane V4.4.4"
+#define THISFIRMWARE "Soomvi Plane V4.4.4"
 
 // the following line is parsed by the autotest scripts
 #define FIRMWARE_VERSION 4,4,4,FIRMWARE_VERSION_TYPE_OFFICIAL

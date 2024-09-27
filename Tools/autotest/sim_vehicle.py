5379dabe4520ceea6e6f340a44e75c1b4f09eeeb
Office Desktop WSL <jhkang@soomvi.com>
2024-09-27 15:58:48 +0900

    sitl host ip address fix

--- Tools/autotest/sim_vehicle.py
+++ Tools/autotest/sim_vehicle.py
@@ -771,14 +771,16 @@ def start_mavproxy(opts, stuff):
 
     for i in instances:
         if not opts.no_extra_ports:
-            ports = [p + 10 * i for p in [14550, 14551]]
+#ports = [p + 10 * i for p in [14550, 14551]]
+            ports = [p + 10 * i for p in [14550]]
             for port in ports:
                 if os.path.isfile("/ardupilot.vagrant"):
                     # We're running inside of a vagrant guest; forward our
                     # mavlink out to the containing host OS
                     cmd.extend(["--out", "10.0.2.2:" + str(port)])
                 else:
-                    cmd.extend(["--out", "127.0.0.1:" + str(port)])
+#cmd.extend(["--out", "127.0.0.1:" + str(port)])
+                    cmd.extend(["--out", "192.168.0.92:" + str(port)])
 
         if not opts.mcast:
             if opts.udp:
